c4df5a28058feac68bf37354627fa29aa8fac78b
Balaji7259 <balaji.techy22@gmail.com>
2025-06-24 14:17:56 +0530

    ALL DONE

---
+++ WEB DEVELOPMENT/Sigma-web-dev-course/VIDEO54/script.js
@@ -0,0 +1,15 @@
+alert("Hello, World!");
+
+console.log("Code is running...")
+console.log("Code is still running..")
+console.log("code is looking like a wow...")
+
+var a = prompt("Enter a number:");
+var isTrue = confirm("Are you sur want to exit this page?  and blast Computer")
+
+if (isTrue) {
+    console.log("Blasting Computer...");
+}
+else {
+    console.log("Continuing to run the code...");
+}
