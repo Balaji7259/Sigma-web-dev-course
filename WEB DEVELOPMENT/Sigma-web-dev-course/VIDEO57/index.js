2cbca83d65b78f22151d08a307ecb86cc63165f7
Balaji7259 <balaji.techy22@gmail.com>
2025-06-26 14:48:02 +0530

    loops in Javascript

---
+++ WEB DEVELOPMENT/Sigma-web-dev-course/VIDEO57/index.js
@@ -0,0 +1,37 @@
+console.log("Tutorial on Loops")
+let a = 1;
+// console.log(a);
+// console.log(a + 1);
+// console.log(a + 2);
+
+// for (let i = 0; i < 100; i++) //for loop
+// {
+//     console.log(i + a);
+// }
+
+// let obj = {
+//     name: "balaji",
+//     role: "developer",
+//     Company: "Shetty s"
+// } 
+
+// // for (let key in obj){
+// //     const ele = obj[key];            // for in loop
+// //     console.log(key, ele);
+// // }
+
+// for (const iterator of "balaji") { 
+//     console.log(iterator);          // for of loop
+// }
+
+// let i = 15;         // while loop
+// while (i < 20){
+//     console.log(i+1);
+//     i++;
+// }
+
+let i = 15;         // do while loop
+do {
+    console.log(i+1);
+    i++;
+} while (i < 20);
