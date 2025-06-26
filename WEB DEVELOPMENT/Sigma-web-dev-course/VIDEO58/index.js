b5d443a6bef5c4d0762bded94192a0b455a880e1
Balaji7259 <balaji.techy22@gmail.com>
2025-06-26 15:41:48 +0530

    JavaScript Functions

---
+++ WEB DEVELOPMENT/Sigma-web-dev-course/VIDEO58/index.js
@@ -0,0 +1,26 @@
+console.log("Tutorial for Functions in JavaScript");
+
+// function nice(name){
+//     console.log("Hey " + name + " is a nice name");
+//     console.log("Hey " + name + " is looing Good");
+//     console.log("hello, This is " + name+"\n");
+// }
+
+// // nice("Balaji")
+// // nice("Suresh")
+
+// function sum(a,b,c=0){
+//     return a + b + c;
+// }
+
+// console.log("Sum of 2 and 3 is " + sum(2,3));
+// console.log("Sum of 2 and 3 is " + sum(5,3));
+// console.log("Sum of 2 and 3 is " + sum(2,3,4));
+
+const func1 = (x) => {
+    console.log("I am a arrow function",x);
+}
+func1("Balaji");
+func1("Suresh");
+func1("Ramesh");
+
