1ffed405281e6a9707a519dbd56aa7422dafbba2
Balaji7259 <balaji.techy22@gmail.com>
2025-06-27 05:43:19 +0530

    Exercise - 09

---
+++ WEB DEVELOPMENT/Sigma-web-dev-course/VIDEO59/index.js
@@ -0,0 +1,14 @@
+/* Create a Faulty Calculator
+
+This Faulty Calculator does Following:
+1. It takes two numbers as input from the user.
+2. It performs wrong operations as Follows:
+
++ --> -
+* --> +
+- --> /
+/ --> **
+
+It Performs Wrong Operations 10% of the Times*
+
+*/
