87cf51a997e40acc441723930e0a4045ec9fd19b
Balaji7259 <balaji.techy22@gmail.com>
2025-07-01 19:32:05 +0530

    Javascript Document Object Model

---
+++ WEB DEVELOPMENT/Sigma-web-dev-course/VIDEO67/script.js
@@ -0,0 +1,51 @@
+console.log("Hello World")
+
+document.body.children
+
+document.body
+
+document.body.childNodes
+
+document.body.childNodes[0]
+
+document.body.childNodes[1]
+
+document.body.childNodes[3]
+
+document.body.childNodes[2]
+
+document.body.childNodes[1].childNodes[1]
+
+document.body.childNodes[1].childNodes[2]
+
+document.body.childNodes[1].childNodes[5]
+
+document.body.childNodes[1].childNodes
+
+document.body.lastChild
+
+
+document.body.lastChild[1]
+
+let cont = document.body.childNodes[1]
+
+cont
+
+cont.firstElementchild
+
+cont.firstElementChild
+
+cont.lastElementChild
+
+
+// cont.lastElementChild.style.color = "red"
+
+// cont.lastElementChild.style.backgroundColor = "green"
+
+cont.lastElementChild.parentElement
+
+document.body.firstElementChild
+
+document.body.firstElementChild.children
+
+document.body.firstElementChild.childNodes
