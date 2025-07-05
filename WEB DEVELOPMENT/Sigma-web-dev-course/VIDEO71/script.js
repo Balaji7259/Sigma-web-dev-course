a9679213defd1fda289f0c7a31b9357665bdd368
Balaji7259 <balaji.techy22@gmail.com>
2025-07-05 16:14:57 +0530

    JavaScript Selectby, Inner HTML, Inner Text., Tagname

---
+++ WEB DEVELOPMENT/Sigma-web-dev-course/VIDEO71/script.js
@@ -0,0 +1,18 @@
+document.querySelector(".container").innerText
+
+document.querySelector(".container").innerHTML
+
+document.querySelector(".container").outerHTML
+
+document.querySelector(".container").tagName
+
+document.querySelector(".container").tagNode
+
+document.querySelector(".container").nodeName
+
+document.querySelector(".box").innerHTML = "Hey I am a Balaji"
+
+document.querySelector(".container").className
+
+document.querySelector(".container").classList.add("Balaji")
+
